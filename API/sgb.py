2d47116c01c7215d950644457caf35b618cf9440
IsmaeRLGV <IsmaeRLGV@gmail.com>
2014-04-19 15:35:14 -0430

    Arreglando unas cositas...

--- API/sgb.py
+++ API/sgb.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import arrays,privs
-
 class conv_type:
 	def __init__(self, i):
 		self.i=i
@@ -23,7 +22,6 @@ class conv_type:
 			return conv
 		if type(self.i) == type(c):
 			return "%s" % self.i
-
 class game:
 	def __init__(self, user,i=None):
 		self.user=user
@@ -38,25 +36,16 @@ class game:
 			if self.i.find("+")!=-1:
 				arrays.DB_user[self.j[1]][3]+=int(self.i.split("+")[1])
 			elif self.i.find("-")!=-1:
-				arrays.DB_user[self.j[1]][3]+=int(self.i.split("-")[1])
-
-def top(self):
-	rst=[]
-	q=game('PTS', self.list).pts_ls()
-	for i in q:
-		ii=conv_type(i).conv_int()
-		rst.append(ii)
-	for i in range(1, len(rst)):
-		for j in range(0, len(rst)-i):
-			if rst[j[0]] > rst[j+1[0]]:
-				elemento=rst[j]
-				rst[j]=rst[j+1]
-				rst[j+1]=elemento
-	return rst			
+				arrays.DB_user[self.j[1]][3]+=int(self.i.split("-")[1])		
 	
-def i():
-	for i in range(1, len(lista)):
-		if lista[j][0] > lista[j+1][0]:
-			elemento=lista[j]
-			lista[j]=lista[j+1]
-			lista[j+1]=elemento
+def top():
+	cList=arrays.DB_user
+	for i in range(1, len(cList)):
+		for j in range(0, len(cList)-i):
+			if cList[j][3] > cList[j+1][3]:
+				elemento=cList[j]
+				cList[j]=cList[j+1]
+				cList[j+1]=elemento
+	
+	cList.reverse()
+	return cList
